a1c8199088eb2c28c3ccee747bfdec8d7810dcf3
Divya Prakash Jha <dpjha84@gmail.com>
2019-06-29 15:03:24 +0530

    Handled null and empty job list tests

---
+++ JobScheduler/JobScheduler.Tests/JobSchedulerFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JobScheduler.Tests
+{
+    [TestClass]
+    public class JobSchedulerFixture
+    {
+        [TestMethod]
+        public void Test_Null_Job_List_Returns_Empty_Sequence()
+        {
+            var scheduler = new JobSceduler();
+            string sequence = scheduler.Schedule(null);
+            Assert.IsTrue(sequence == string.Empty);
+        }
+
+        [TestMethod]
+        public void Test_Empty_Job_List_Returns_Empty_Sequence()
+        {
+            var jobList = new List<string>();
+            var scheduler = new JobSceduler();
+            string sequence = scheduler.Schedule(jobList);
+            Assert.IsTrue(sequence == string.Empty);
+        }
+    }
+}
---
+++ JobScheduler/JobScheduler/JobSceduler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScheduler
+{
+    public class JobSceduler
+    {
+        public string Schedule(List<string> jobList)
+        {
+            if (jobList == null || jobList.Count == 0)
+                return string.Empty;
+            return null;
+        }
+    }
+}
